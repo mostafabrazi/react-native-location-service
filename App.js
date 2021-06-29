0472f3aa729522fcc829c0226c576b63c61a221d
Mostafa <brazimostafa@gmail.com>
2021-06-29 23:05:01 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { NativeModules, Button } from 'react-native';
 
-
 const App = () => {
   const onPress = () => {
+    // native module
     const { BackgroundService } = NativeModules;
     BackgroundService.createBackgroundThread('update_positions', '8');
     console.log('We will invoke the native module here!');
   };
-
   return (
     <Button
       title="Click to invoke your native module!"
